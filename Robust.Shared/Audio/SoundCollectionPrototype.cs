e042e76cd10a3d5ce4b783f9ecb3799fd6c62a7a
Tayrtahn <tayrtahn@gmail.com>
2025-07-15 15:09:49 -0400

    Add AllowStereo boolean to SoundCollectionPrototype

--- Robust.Shared/Audio/SoundCollectionPrototype.cs
+++ Robust.Shared/Audio/SoundCollectionPrototype.cs
@@ -13,6 +13,17 @@ public sealed partial class SoundCollectionPrototype : IPrototype
     [IdDataField]
     public string ID { get; private set; } = default!;
 
+    /// <summary>
+    /// Are the files in this collection allowed to have more than one channel?
+    /// </summary>
+    /// <remarks>
+    /// This should be left false for collections that can be played positionally,
+    /// because playing a stereo audio file positionally causes a clientside error.
+    /// Set this to true for audio that is only played globally, like lobby music.
+    /// </remarks>
+    [DataField]
+    public bool AllowStereo;
+
     [DataField("files")]
     public List<ResPath> PickFiles { get; private set; } = new();
 }
